305e98d9dac68b66a4cd4e22af43561174540634
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-23 22:54:35 +0600

    tsup.config.ts modified

---
+++ tsup.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  // 1. Entry points for the bundler
+  entry: ['src/server.ts'],
+
+  // 2. Output formats: CommonJS and ES Modules
+  format: ['cjs', 'esm'],
+  target:'esnext',
+  outDir:'dist',
+
+  
+
+  // 4. Clear the output directory before each build
+  clean: true,
+  bundle:true,
+  splitting:false,
+
+  // 5. Generate source maps for debugging
+  sourcemap: true,
+
+  // 6. Minify output code for production
+  minify: true,
+
+  // 7. Prevent bundling external dependencies
+  external: ['react', 'lodash'],
+  banner:{
+    js:`
+    import {createRequire} from 'module';
+    const require=createRequire(import.meta.url);
+    `,
+  },
+});
