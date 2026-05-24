257ba8c909bcfca54d01e9bde482a73969d48bdb
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-24 22:46:31 +0600

    Error Response Structure Modified

--- src/utility/sendResponse.ts
+++ src/utility/sendResponse.ts
@@ -4,19 +4,28 @@ type TResponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string;
-  data?: T; // Received any type of data, making it flexible for different responses
-  error?:unknown;
+  data?: T;
+  errors?: unknown;
 };
 
 const sendResponse = <T>(
   res: Response,
   data: TResponse<T>
 ) => {
-  return res.status(data.statusCode).json({
+
+  return res.status(
+    data.statusCode
+  ).json({
     success: data.success,
     message: data.message,
-    ...(data.data != undefined && { data: data.data }),
-    ...(data.error !== undefined && {error: data.error,})
+
+    ...(data.data !== undefined && {
+      data: data.data,
+    }),
+
+    ...(data.errors !== undefined && {
+      errors: data.errors,
+    }),
   });
 };
 
